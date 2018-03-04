ea3dce0a50e48ea35e86848ceeb5af6b871477da
Christopher Dorst <chris@dorst.email>
2018-03-03 21:12:02 -0600

    Add project files.

---
+++ DevOps.Build.Tools.LocalNuGetSource.AfterBuild/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DevOps.Build.Tools.LocalNuGetSource.AfterBuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
