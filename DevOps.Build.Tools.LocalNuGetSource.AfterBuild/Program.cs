fac0aa0565895b8a580d1e85cc6109c8a77c6e38
Christopher Dorst <chris@dorst.email>
2018-03-04 00:43:45 -0600

    Add "dotnet after.dll" tool to upload NuGet packages to blob storage

--- DevOps.Build.Tools.LocalNuGetSource.AfterBuild/Program.cs
+++ DevOps.Build.Tools.LocalNuGetSource.AfterBuild/Program.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.Storage;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using static DevOps.ContentDelivery.Functions.UploadAzureBlob.BlobUploader;
+using static System.Environment;
+using static System.IO.Compression.ZipFile;
+using static System.IO.Directory;
+using static System.IO.File;
+using static System.IO.Path;
 
 namespace DevOps.Build.Tools.LocalNuGetSource.AfterBuild
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var token = args.FirstOrDefault() ?? throw new ArgumentNullException("NamespacePrefix");
+            var containerName = args[1] ?? throw new ArgumentNullException("ContainerName");
+            var tmpDir = args[2] ?? throw new ArgumentNullException("TempDirectory");
+
+            var account = CloudStorageAccount.Parse(
+                GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING"));
+            var container = account.CreateCloudBlobClient().GetContainerReference(containerName);
+
+            // Upload each nupkg to CDN
+            var dir = GetEnvironmentVariable("APPVEYOR_BUILD_FOLDER");
+            var files = EnumerateFiles(dir, " *.nupkg", SearchOption.AllDirectories);
+            foreach (var file in files)
+            {
+                var name = file.Split('/').Last();
+                var zip = GetTempFileName();
+                Console.WriteLine($"Uploading {zip}");
+                Delete(tmpDir, true);
+                CreateDirectory(tmpDir);
+                Copy(file, Combine(tmpDir, name));
+                CreateFromDirectory(tmpDir, zip);
+                await Upload(container, name, zip);
+                File.Delete(zip);
+            }
         }
     }
 }
